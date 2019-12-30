cdc8b314a067873612baddc5f291b53ca92652f0
Dody Gunawinata <dodyg@silverkeytech.com>
2019-12-31 00:02:58 +0200

    Terminal middleware sample (WIP)

---
+++ samples/Middleware-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var builder = WebApplicationHost.CreateDefaultBuilder(args);
+        var app = builder.Build();
+        app.UseRouting();
+        app.Use(async (context, next) => 
+        {
+            context.Items["Greetings"] = "Hello world";
+            context.Items["Path"] = context.GetEndpoint().ToString();
+            await next.Invoke();
+        });
+        
+        app.Use(async (context, next) =>
+        {
+            await context.Response.WriteAsync($"{context.Items["Greetings"]} for {context.Items["Path"]}");
+        });
+
+        await app.RunAsync();
+    }
+}
